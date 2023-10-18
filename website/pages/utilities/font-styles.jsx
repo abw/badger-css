041f1cb82afaeef3e688797370d7f2c6ed4fad61
Andy Wardley <abw@wardley.org>
2023-10-18 08:29:18 +0100

    Font styles and weights

---
+++ website/pages/utilities/font-styles.jsx
@@ -0,0 +1,19 @@
+import React       from 'react'
+import Example     from '../../site/Example.jsx'
+import Styles      from '../../snippets/styles/styles.html?raw'
+
+const FontStyles = () =>
+  <div className="prose">
+    <h1>Font Styles</h1>
+    <p>
+      The following classes are defined for common font styles.
+    </p>
+    <Example
+      html={Styles}
+      caption="Font Styles"
+      expand
+    />
+  </div>
+
+
+export default FontStyles
---
+++ website/pages/utilities/font-weights.jsx
@@ -0,0 +1,80 @@
+import React       from 'react'
+import Example     from '../../site/Example.jsx'
+import Names       from '../../snippets/weights/names.html?raw'
+import Numbers     from '../../snippets/weights/numbers.html?raw'
+import Aliases     from '../../snippets/weights/aliases.html?raw'
+import WeightsSCSS from '../../snippets/sass/weights.scss?raw'
+import Split       from '../../site/Split.jsx'
+import CodeBlock from '../../site/CodeBlock.jsx'
+
+const WeightExamples = () =>
+  <div className="prose">
+    <h1>Font Weights</h1>
+    <h2>Weight Names</h2>
+    <Split>
+      <p>
+        The <code>weight-&lt;name&gt;</code> classes can be used to set the font weight.
+      </p>
+      <p>
+        The values for <code>&lt;name&gt;</code> can be
+        <code>thin</code>, <code>extra-light</code>, <code>light</code>,{' '}
+        <code>regular</code>, <code>medium</code>, <code>semi-bold</code>,{' '}
+        <code>bold</code>, <code>extra-bold</code> or <code>black</code>.
+      </p>
+    </Split>
+
+    <Example
+      html={Names}
+      caption="Weight Names"
+      expand
+    />
+
+    <h2>Weight Numbers</h2>
+
+    <Split>
+      <p>
+        Alternately you can use the corresponding weight numbers.
+      </p>
+      <p>
+        Those numbers can be{' '}
+        <code>100</code>, <code>200</code>, <code>300</code>,{' '}
+        <code>400</code>, <code>500</code>, <code>600</code>,{' '}
+        <code>700</code>, <code>800</code> or <code>900</code>.
+      </p>
+    </Split>
+    <Example
+      html={Numbers}
+      caption="Weight Numbers"
+      expand
+    />
+
+    <h2>Weight Aliases</h2>
+    <p>
+      For the common cases...
+    </p>
+    <Example
+      html={Aliases}
+      caption="Weight Aliases"
+      expand
+    />
+
+    <h2>Custom Weights</h2>
+    <Split>
+      <p>
+        You can set the <code>$font-weights</code> SASS variable if you want
+        to define a different set of font weights.
+      </p>
+    </Split>
+
+    <CodeBlock
+      code={WeightsSCSS}
+      caption="Font Weights"
+      language="scss"
+      className="mar-b-8"
+      expand
+    />
+
+  </div>
+
+
+export default WeightExamples
