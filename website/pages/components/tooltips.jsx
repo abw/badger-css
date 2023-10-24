0792230016ce287cc082f64d2baee985c740376f
Andy Wardley <abw@wardley.org>
2023-10-24 11:36:43 +0100

    Switched to tooltips from badger-ui with some tweaks

--- website/pages/components/tooltips.jsx
+++ website/pages/components/tooltips.jsx
@@ -1,31 +1,48 @@
 import React     from 'react'
 import Example   from '../../site/Example.jsx'
-import Tooltips  from '../../snippets/html/tooltips.html?raw'
-import Custom    from '../../snippets/html/tooltips-custom.html?raw'
+import Tooltip   from '../../snippets/html/tooltip.html?raw'
+import Position  from '../../snippets/html/tooltip-position.html?raw'
+import Corners   from '../../snippets/html/tooltip-corner.html?raw'
+import Custom    from '../../snippets/html/tooltip-custom.html?raw'
 
 const TooltipsPage = () =>
   <div className="prose">
     <h1>Tooltips</h1>
     <p>
-      The <a href="https://www.npmjs.com/package/@zkreations/tooltips">Tooltip.css</a>{' '}
-      library by <a href="https://github.com/zkreations">zkreations</a> is
-      included for displaying tooltips.
-    </p>
-    <p>
-      The only changes to the default settings are to provide different colors
-      for light and dark modes.  This is done by providing values for the{' '}
-      <code>--tts-background</code> and <code>--tts-color</code> variables.
+      Add the <code>data-tooltip</code> attribute to an element to enable a
+      tooltip.  The tooltip text should be defined in the <code>aria-label</code>{' '}
+      property so that it&apos;s also accessible to screen readers.
     </p>
     <Example
-      html={Tooltips}
+      html={Tooltip}
       language="html"
-      caption="Tooltips"
+      caption="Tooltip"
+    />
+    <h2>Tooltip Position</h2>
+    <p>
+      You can set the <code>data-tooltip</code> attribute to any of{' '}
+      <code>left</code>, <code>right</code>, <code>top</code> or{' '}
+      <code>bottom</code> to set the tooltip position.
+    </p>
+    <Example
+      html={Position}
+      language="html"
+      caption="Tooltip Position"
+    />
+    <p>
+      You can also combine them as {' '}
+      <code>top left</code>, <code>top right</code>, <code>bottom left</code>{' '}
+      and <code>bottom right</code>.
+    </p>
+    <Example
+      html={Corners}
+      language="html"
+      caption="Tooltip Corners"
     />
 
     <p>
-      You can set any of the supported{' '}
-      <a href="https://www.npmjs.com/package/@zkreations/tooltips#customize">CSS variables</a>{' '}
-      to customise the tooltips further.
+      There are a number of SCSS variables that can be used to set the
+      defaults, or you can use CSS variables to create custom tooltip styles.
     </p>
     <Example
       html={Custom}
