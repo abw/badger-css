38cda9f600d39faf6190ff313b42a7355dfcc7de
Andy Wardley <abw@wardley.org>
2023-10-27 13:04:29 +0100

    Color input

--- website/pages/forms/text.jsx
+++ website/pages/forms/text.jsx
@@ -1,12 +1,13 @@
-import React    from 'react'
-import Example  from '../../site/Example.jsx'
-import Text     from '../../snippets/form/text.html?raw'
-import TextWide from '../../snippets/form/text-wide.html?raw'
-import TextGrid from '../../snippets/form/text-grid.html?raw'
+import React     from 'react'
+import Example   from '../../site/Example.jsx'
+import Text      from '../../snippets/form/text.html?raw'
+import TextWide  from '../../snippets/form/text-wide.html?raw'
+import TextGrid  from '../../snippets/form/text-grid.html?raw'
+import TextTypes from '../../snippets/form/text-types.html?raw'
 import Link from '../../ui/Link.jsx'
 
 const TextInputs = () =>
-  <div>
+  <div className="prose">
     <h1>Text Input</h1>
     <p>
       Text inputs are perhaps the most common kind of input.
@@ -32,6 +33,19 @@ const TextInputs = () =>
       html={TextGrid}
       language="html"
       caption="Text Grid"
+    />
+    <p>
+      You can set the <code>type</code> to one of the other text-based input
+      types (e.g. <code>password</code>, <code>number</code>,{' '}
+      <code>telephone</code>, <code>date</code>, <code>time</code>, etc).
+      There&apos;s also some styling provided for the <code>color</code> type
+      although it&apos;s notoriously hard to style and the appearance can
+      vary on different browsers, devices and operating systems.
+    </p>
+    <Example
+      html={TextTypes}
+      language="html"
+      caption="Other Input Types"
     />
 
   </div>
