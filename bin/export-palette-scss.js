ae1eb13f4f5096806c6f38acc036c06385dacb96
Andy Wardley <abw@wardley.org>
2023-10-31 15:55:35 +0000

    Added SCSS palette export functions from badger-color

---
+++ bin/export-palette-scss.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { commandLine, exportSCSSPalette, BadgerCSSVersion } from '../lib/index.js'
+
+const script      = 'bin/export-palette-scss.js'
+const version     = BadgerCSSVersion
+const description = 'Generates SCSS configuration files from a palette.'
+
+async function main() {
+  await commandLine({
+    script,
+    version,
+    description,
+    generator: exportSCSSPalette
+  })
+}
+main()
+
