1bc536d0a548107088dac0577b1e1619bf6b2a36
Andy Wardley <abw@wardley.org>
2023-11-18 19:19:58 +0000

    Icon animations

--- website/pages/components/icons.jsx
+++ website/pages/components/icons.jsx
@@ -1,15 +1,17 @@
 import React      from 'react'
 import Example    from '@/site/Example.jsx'
 import Icon       from '@/snippets/icons/icon.html?raw'
-import IconSizes  from '@/snippets/icons/icon-sizes.html?raw'
-import IconColors from '@/snippets/icons/icon-colors.html?raw'
-import IconStyle  from '@/snippets/icons/icon-style.html?raw'
-import IconCustom from '@/snippets/icons/icon-custom.html?raw'
+import IconSizes  from '@/snippets/icons/sizes.html?raw'
+import IconColors from '@/snippets/icons/colors.html?raw'
+import IconStyle  from '@/snippets/icons/style.html?raw'
+import IconCustom from '@/snippets/icons/custom.html?raw'
+import IconSpin   from '@/snippets/icons/spin.html?raw'
+import IconBeat   from '@/snippets/icons/beat.html?raw'
 import Note       from '@/site/Note.jsx'
 import Split from '@/site/Split.jsx'
 
 const IconsExamples = () =>
-  <div className="prose flow">
+  <div className="flow prose">
     <h1>Icons</h1>
     <p>
       Some basic styles are included for SVG icons. Add the <code>icon</code>{' '}
@@ -20,6 +22,8 @@ const IconsExamples = () =>
       language="html"
       caption="Icon"
     />
+
+    <h2>Sizing</h2>
     <p>
       Icons are sized in em units meaning they scale with text in different
       sizes.
@@ -29,6 +33,8 @@ const IconsExamples = () =>
       language="html"
       caption="Icon Sizes"
     />
+
+    <h2>Fill and Stroke Colors</h2>
     <p>
       SVG icons can use <code>currentColor</code> for either the{' '}
       <code>fill</code> or <code>stroke</code> properties to inherit the
@@ -65,6 +71,8 @@ const IconsExamples = () =>
       language="html"
       caption="Icon Style"
     />
+
+    <h2>Custom Styling</h2>
     <p>
       You can define your own styles to target different elements in an
       SVG icon.  This example illustrated the use of generic{' '}
@@ -85,6 +93,50 @@ const IconsExamples = () =>
       language="html"
       caption="Icon Style"
     />
+
+    <h2>Animation Effects</h2>
+    <Split align="end">
+      <div>
+        <p>
+          The <code>spin</code> CSS class will make the icon spin.  Ideal for
+          spinners!  Add the <code>reverse</code> class to reverse the direction.
+        </p>
+        <p>
+          The default animation time is 2s.  You can add the <code>fast</code> CSS
+          class to double the speed (animation time is 1s) or <code>slow</code> to
+          halve it (animation time is 4s).  Or you can set the{' '}
+          <code>--animation-time</code> property to anything you like.
+        </p>
+      </div>
+      <Note>
+        These animations are disabled if the user&apos;s browser has
+        the <code>prefers-reduced-motion</code> option set to <code>reduce</code>.
+      </Note>
+    </Split>
+    <Example
+      html={IconSpin}
+      language="html"
+      caption="Icon Spin"
+    />
+
+    <Split align="end">
+      <div>
+        <p>
+          The <code>beat</code> CSS class will make the icon beat.
+        </p>
+        <p>
+          The default animation time here is 1s.  The <code>fast</code> class
+          doubles the speed (animation time is 0.5s) and the <code>slow</code> class
+          halves it (animation speed is 2s).
+        </p>
+      </div>
+    </Split>
+    <Example
+      html={IconBeat}
+      language="html"
+      caption="Icon Beat"
+    />
+
   </div>
 
 export default IconsExamples
