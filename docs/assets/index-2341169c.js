c132584bf41595ec57d4a85ac55a43432d515666
Andy Wardley <abw@wardley.org>
2023-11-30 19:09:08 +0000

    Badger picture URL fix

--- docs/assets/index-2341169c.js
+++ docs/assets/index-2341169c.js
@@ -4383,7 +4383,7 @@ $fonts: (
   Some text goes here
 </label>
 `,xR=`<div class="grid overlap">
-  <img src="/badger-forest.jpg" width="100%"/>
+  <img src="/badger-css/badger-forest.jpg" width="100%"/>
   <h1 class="fgc-100 pad-2 x3 weight-600" style="width: 70%">
     Anyone for mushrooms?
   </h1>
