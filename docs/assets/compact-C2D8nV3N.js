192f471da813d4ac8fd2313d270a4fa3fab8c19a
Andy Wardley <abw@wardley.org>
2024-09-26 10:35:09 +0100

    Docs build

---
+++ docs/assets/compact-C2D8nV3N.js
@@ -0,0 +1,16 @@
+const a=`<div class="info alert border compact">
+  This is a compact info alert with a border
+</div>
+<div class="success alert border compact">
+  <h3>Success!</h3>
+  This is a success alert with a border and a headline
+</div>
+<div class="small warning alert border compact">
+  <h3>Warning!</h3>
+  This is a small compact warning alert with a border and a headline
+</div>
+<div class="smaller error alert border compact">
+  <div class="headline">Error!</div>
+  This is a smaller compact error alert with a border and a headline
+</div>
+`;export{a as default};
---
+++ docs/assets/span-CrO1kFvi.js
@@ -0,0 +1,10 @@
+const s=`<div class="grid-3 gap-4 text-center font-mono">
+  <div class="inverse green  pad-a-2">X</div>
+  <div class="inverse teal   pad-a-2 cols-2">cols-2</div>
+  <div class="inverse blue   pad-a-2 cols-2">cols-2</div>
+  <div class="inverse indigo pad-a-2 rows-2">rows-2</div>
+  <div class="inverse violet pad-a-2">Y</div>
+  <div class="inverse purple pad-a-2">Z</div>
+  <div class="inverse pink   pad-a-2 cols-3">cols-3</div>
+</div>
+`;export{s as default};
