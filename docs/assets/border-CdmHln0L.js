8dc88968406601e3367153e1083cd89ade31021e
Andy Wardley <abw@wardley.org>
2024-12-06 11:07:59 +0000

    Docs build

--- docs/assets/border-CdmHln0L.js
+++ docs/assets/border-CdmHln0L.js
@@ -1,6 +1,7 @@
 const a=`<div class="flex wrap middle gap-2">
   <span class="badge bdw-4">Border width 4</span>
   <span class="badge bdr-2">Border radius 2</span>
+  <span class="badge square">Square Badge</span>
   <span class="badge bdc-50">Border color stop 50</span>
 </div>
 `;export{a as default};
---
+++ docs/assets/prefix-CG59u0ci.js
@@ -0,0 +1,21 @@
+const a=`<div class="flex baseline wrap gap-4">
+  <div class="badge">
+    <div class="prefix">Prefix</div>
+    Badge
+  </div>
+
+  <div class="blue badge">
+    <div class="prefix">Blue</div>
+    Badge
+  </div>
+
+  <div class="small compact red badge">
+    <div class="prefix">Small Red</div>
+    Compact Badge
+  </div>
+
+  <div class="large orange badge bdr-1">
+    <div class="prefix">Large Orange</div>
+    Badge
+  </div>
+</div>`;export{a as default};
